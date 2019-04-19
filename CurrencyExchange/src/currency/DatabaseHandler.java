47f443ac58e56f45488e8ff611ff2970c1f7dc4f
mtesluk <34014200+mtesluk@users.noreply.github.com>
2019-04-19 21:31:42 +0200

    Create DatabaseHandler.java

---
+++ CurrencyExchange/src/currency/DatabaseHandler.java
@@ -0,0 +1,17 @@
+public class DatabaseHandler {
+	static public void deleteAll() {
+		;
+	}
+	
+	static public void get() {
+		;
+	}
+	
+	static public void update() {
+		;
+	}
+	
+	static public void create() {
+		;
+	}
+}
