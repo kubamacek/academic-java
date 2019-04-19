d784b52f72a0c350703ab8e457e131cf1efa358a
mtesluk <34014200+mtesluk@users.noreply.github.com>
2019-04-19 21:35:47 +0200

    Create BindConverter.java

---
+++ CurrencyExchange/src/currency/BindConverter.java
@@ -0,0 +1,15 @@
+public class Converter extends org.jdesktop.beansbinding.Converter<String, String>{
+	
+	@Override
+	public String convertForward(String arg0) {
+		String result = "";
+		result = arg0.replaceAll("\\D+","");
+		return result;
+	}
+
+	@Override
+	public String convertReverse(String arg0) {
+		return arg0;
+	}
+
+}
